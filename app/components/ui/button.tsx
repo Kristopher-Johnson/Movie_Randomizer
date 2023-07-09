819d1ba23504c4d15d09f094300016fd397fd625
Kristopher-Johnson <kristopher.mj@hotmail.com>
2023-07-09 23:41:52 +0100

    Added form for adding movie to list

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,10 +1,14 @@
-import React, { ReactElement, ReactPortal } from "react";
+import React, { ReactElement, ReactNode, ReactPortal } from "react";
 import { PropsWithChildren } from "react";
 import classes from "./button.module.css";
 
-const Button = (props: PropsWithChildren) => {
+const Button = (props: any) => {
   return (
-    <button className={classes["button-40"]} role="button">
+    <button
+      className={classes["button-40"]}
+      onClick={props.onClick}
+      role="button"
+    >
       {props.children}
     </button>
   );
