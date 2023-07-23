919ed288a7006c0884f6b4a61e84332496331334
Kristopher Johnson <kris.mj@outlook.com>
2023-07-24 00:10:20 +0100

    Added abitlity to register account

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,55 +1,66 @@
 "use client";
 
-import React from "react";
-import TextField from "../components/ui/TextField";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
 import Link from "next/link";
 import classes from "./Login.module.css";
 import Button from "../components/ui/Button";
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { doc, collection, addDoc, getDoc, getDocs } from "firebase/firestore";
-import db from "../../firebase";
+import { getFirestore } from "firebase/firestore";
+import app from "../../firebase";
 
-// const db = getFirestore(firebase);
+const db = getFirestore(app);
 
-function addUser(
-  email: string,
-  firstname: string,
-  lasename: string,
-  password: string
-) {
-  try {
-    const docRef = addDoc(collection(db, "users"), {
-      first: { firstname },
-      last: { lasename },
-      email: { email },
-      password: { password },
-    });
-    console.log("Document written with ID: ", docRef);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-}
+// async function addUser(
+//   email: string,
+//   firstname: string,
+//   lasename: string,
+//   password: string
+// ) {
+//   try {
+//     const docRef = await addDoc(collection(db, "users"), {
+//       first: { firstname },
+//       last: { lasename },
+//       email: { email },
+//       password: { password },
+//     });
+//     console.log("Document written with ID: ", docRef.id);
+//   } catch (e) {
+//     console.error("Error adding document: ", e);
+//   }
+// }
 
-const docRef = doc(db, "users", "6x7unXdKXovZjJHnRNe9");
-const docSnap = await getDoc(docRef);
+// async function getUsers() {
+//   const querySnapshot = await getDocs(collection(db, "users"));
+//   console.log(querySnapshot);
+// }
 
-if (docSnap.exists()) {
-  console.log("Document data:", docSnap.data());
-} else {
-  // docSnap.data() will be undefined in this case
-  console.log("No such document!");
-}
+// async function getUser() {
+//   const docRef = doc(db, "users", "7h1TJWZIQcfP0Lh4OAsK");
+//   const docSnap = await getDoc(docRef);
 
-// console.log("here");
+//   if (docSnap.exists()) {
+//     console.log("Document data:", docSnap.data());
+//   } else {
+//     // docSnap.data() will be undefined in this case
+//     console.log("No such document!");
+//   }
+// }
 
-const querySnapshot = await getDocs(collection(db, "users"));
-console.log(querySnapshot.docs);
-
+// getUsers();
+// getUser();
 // console.log("test");
 
 export default function Login() {
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onClickLoginHandler() {
+    console.log(userName);
+    console.log(password);
+  }
+
   return (
     <div className={classes["page_center"]}>
       <form className={classes["parent"]}>
@@ -58,16 +69,16 @@ export default function Login() {
         </div>
         <div className={classes["div1"]}>
           <label>Username</label>
-          <TextField inputID={"input1"} />
+          <TextField onChange={(e) => setUsername(e.target.value)} />
         </div>
         <div className={classes["div2"]}>
           <label>Password</label>
-          <TextField inputID={"input2"} />
+          <TextField onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div className={classes["div3"]}>
           <div className={classes["button-1"]}>
-            <Link href={"/user/randomizer"}>
-              <Button>Login</Button>
+            <Link href={""}>
+              <Button onClick={onClickLoginHandler}>Login</Button>
             </Link>
           </div>
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,31 +1,119 @@
-import React from "react";
-import TextField from "../components/ui/TextField";
+"use client";
+
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
 import Link from "next/link";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, collection, addDoc, getDoc, getDocs } from "firebase/firestore";
+import Button from "../components/ui/Button";
+import classes from "./Register.module.css";
+import validator from "validator";
+import app from "../../firebase";
+import { getFirestore } from "firebase/firestore";
 
 export default function Register() {
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  //Adds user to user database and firebase authentication database
+  async function registerUser(
+    email: string,
+    username: string,
+    password: string
+  ) {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        username: { username },
+        email: { email },
+        movieList: [],
+      });
+      console.log("User added with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding user");
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+  function validUsername(username: string) {
+    return true;
+  }
+
+  function validPassword(password: string, repeatPassword: string) {
+    validator.isStrongPassword("minLength: 8");
+    return true;
+  }
+
+  function validateEmail(email: string) {
+    if (validator.isEmail(email)) {
+      // userSignInMethods.length > 0;
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function onClickRegisterHandler() {
+    if (
+      email != "" &&
+      password != "" &&
+      repeatPassword != "" &&
+      username != ""
+    ) {
+      if (
+        validateEmail(email) &&
+        validPassword(password, repeatPassword) &&
+        validUsername(username)
+      ) {
+        registerUser(email, username, password);
+      }
+    } else {
+      alert("Please fill out the form");
+    }
+  }
+
   return (
-    <div>
-      <div>
-        <h1>Username</h1>
-        <TextField inputID={"input1"} />
-      </div>
-      <div>
-        <h1>Email</h1>
-        <TextField inputID={"input2"} />
-      </div>
-      <div>
-        <h1>Password</h1>
-        <TextField inputID={"input3"} />
-      </div>
-      <div>
-        <h1>Repeat Password</h1>
-        <TextField inputID={"input4"} />
-      </div>
-      <div>
-        <Link href={"/user/randomizer"}>
-          <button>Register</button>
-        </Link>
-      </div>
+    <div className={classes["page_center"]}>
+      <form className={classes["parent"]}>
+        <div>
+          <h1>Username</h1>
+          <TextField onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <h1>Email</h1>
+          <TextField onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <h1>Password</h1>
+          <TextField onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div>
+          <h1>Repeat Password</h1>
+          <TextField onChange={(e) => setRepeatPassword(e.target.value)} />
+        </div>
+        <div>
+          <div>
+            <Button onClick={onClickRegisterHandler}>Register</Button>
+          </div>
+          <div>
+            <Link href={"/"}>
+              <Button>Sign in</Button>
+            </Link>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
